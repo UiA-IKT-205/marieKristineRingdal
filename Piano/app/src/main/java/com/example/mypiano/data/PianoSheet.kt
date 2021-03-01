6d55dcc3abecc6f311acdfaa1c3253b265c2cbc1
Marie Kristine Ringdal <mariekr@uia.no>
2021-03-01 10:21:08 +0100

    Added recoder and save notesheet functionality

---
+++ Piano/app/src/main/java/com/example/mypiano/data/PianoSheet.kt
@@ -0,0 +1,10 @@
+package com.example.mypiano.data
+
+data class PianoSheet(val note:String, val startTimeOfNote:Long, val noteTimeDuration:Long){
+
+    override fun toString(): String {
+        return "Note: $note , Started playing at: $startTimeOfNote , Lasted for: $noteTimeDuration"
+    }
+}
+
+
